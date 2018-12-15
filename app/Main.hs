4516ae8461c50858b5d699a6516b0ced69b527b5
Francesco Parolini <parolini.francesco.1@gmail.com>
2018-12-15 19:09:29 +0100

    main that accepts input

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,35 @@ module Main where
 
 import Lib
 
-main :: IO ()
-main = someFunc
+import System.IO
+import Language
+import BaseParser
+import CoreParser
+
+readF :: String -> IO String
+readF fileName = do 
+    inh <- openFile fileName ReadMode
+    prog <- readloop inh
+    hClose inh
+    return prog
+
+readloop :: Handle -> IO [Char]
+readloop inh = do 
+    ineof <- hIsEOF inh
+    if ineof
+        then return []
+        else do
+            x <- hGetLine inh
+            xs <- readloop inh
+            return (x ++ xs)
+
+comp :: [(CoreProgram, Name)] -> CoreProgram
+comp []         = error "Parse error"
+comp [(e,[])]   = e
+comp [(_,rest)] = error ("doesn't use all input: " ++ rest)
+
+main :: IO (Program Name)
+main = do 
+    inp <- readF "input.txt"
+    print $ comp $ parse parseProg inp
+    return (comp (parse parseProg inp))
