cf2442b01970b79980d779a9d7456acf2d9b6359
phreppo <parolini.francesco.1@gmail.com>
2019-02-14 18:05:05 +0100

    prelude definitions

--- src/Language.hs
+++ src/Language.hs
@@ -67,3 +67,19 @@ bindersOf defns = [name | (name, rhs) <- defns]
 
 rhssOf :: [(a,b)] -> [b]
 rhssOf defns = [rhs | (name, rhs) <- defns]
+
+--------------------------------------------------------------------------------
+--                              Prelude Definitions
+--------------------------------------------------------------------------------
+
+preludeDefs :: CoreProgram
+preludeDefs = [
+    ("I", ["x"], EVar "x"),
+    ("K", ["x","y"], EVar "x"),
+    ("K1",["x","y"], EVar "y"),
+    ("S", ["f","g","x"], EAp (EAp (EVar "f") (EVar "x"))
+                             (EAp (EVar "g") (EVar "x"))),
+    ("compose", ["f","g","x"], EAp (EVar "f")
+                                   (EAp (EVar "g") (EVar "x"))),
+    ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f"))
+  ]
